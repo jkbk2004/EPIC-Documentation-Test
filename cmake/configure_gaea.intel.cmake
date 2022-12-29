1075a9bd64d5c2e94b8a71c6ec20ddc936114e4e
jkbk2004 <jong.kim@noaa.gov>
2022-12-29 17:04:56 -0500

    a

---
+++ cmake/configure_gaea.intel.cmake
@@ -0,0 +1,3 @@
+set(PARALLEL_NETCDF ON  CACHE BOOL "Enable parallel NetCDF" FORCE)
+set(DEBUG_LINKMPI   OFF CACHE BOOL "Enable linkmpi option when DEBUG mode is on" FORCE)
+set(MOM6_Extra_FORTRAN_FLAGS "-xsse2")
